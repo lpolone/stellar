0fbf81a98c25ca6a770ec2907d6d4bca7cc159ba
Lucas Polloni <lcs.polloni@gmail.com>
2022-05-19 22:38:28 +0200

    new branch stage

---
+++ scripts/script.js
@@ -0,0 +1,26 @@
+import http from 'k6/http';
+
+import { check, sleep } from 'k6';
+
+
+export const options = {
+
+  stages: [
+
+    { duration: '30s', target: 20 },
+    { duration: '20s', target: 0 },
+
+  ],
+
+};
+
+
+export default function () {
+
+  const res = http.get('https://httpbin.test.k6.io/');
+
+  check(res, { 'status was 200': (r) => r.status == 200 });
+
+  sleep(1);
+
+}
